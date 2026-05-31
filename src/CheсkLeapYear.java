36b700e4f16f4fb43e7068f44fffca576fa2d8f6
XXX <ac4tes@gmail.com>
2026-05-31 15:22:08 +0300

    End tasks whith comment #2

--- src/CheсkLeapYear.java
+++ src/CheсkLeapYear.java
@@ -1,17 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class CheсkLeapYear {
-    public static int targetYear = 2021;
+    public static int checkTargetYear = 2021;
 
     public static boolean chekleapYear() {
-        return ((targetYear > 1584 && targetYear % 4 == 0 && targetYear % 100 != 0) || (targetYear % 400 == 0));
+        return ((checkTargetYear > 1584 && checkTargetYear % 4 == 0 && checkTargetYear % 100 != 0) || (checkTargetYear % 400 == 0));
     }
 
     public static void main(String[] args) {
         if (chekleapYear()) {
-            System.out.println(targetYear + " год является високосным");
+            System.out.println(checkTargetYear + " год является високосным");
         } else {
-            System.out.println(targetYear + " год не является високосным");
+            System.out.println(checkTargetYear + " год не является високосным");
         }
     }
 }
